188c392a230e893c39bb9b3317f19398903587cc
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-03-29 23:14:32 +0330

    chore: install core-js and regenerator-runtime as normal dependencies

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -4,6 +4,15 @@ import icons from 'url:../../img/icons.svg';
 export default class View {
   _data;
 
+  /**
+   * Render the received object to the DOM
+   * @param {Object | Object[]} data The data to be rendered (e.g. recipe)
+   * @param {boolean} [render = true] If false, create markup string instead of rendering to the DOM 
+   * @returns {undefined | string} A markup string is returned if render=false
+   * @this {Object} View instance
+   * @author Ahmad Mohammadirad
+   * @todo Finish implementation
+   */
   render(data , render = true) {
     if (!data || (Array.isArray(data) && data.length === 0))
       return this.renderError();
@@ -43,7 +52,6 @@ export default class View {
       }
     });
   }
-
   renderError(message = this._errorMessage) {
     const markup = `
     <div class="error">
